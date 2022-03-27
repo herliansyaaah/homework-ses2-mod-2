08a997cdc501fe7871f23d6dba50bf338a9ca92b
Harley <suherlisutan@unsera.ac.id>
2022-03-27 17:55:16 +0700

    loop playlist array data

--- src/component/mydata/index.js
+++ src/component/mydata/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const Dataku =({image,title,album,artist}) => {
+const Dataku =({image,title,albums,artists}) => {
     return (
-        <div>
-            <img src={image}/>
-            <label>{artist}</label>
-            <label>{album}</label>
-            <label>{title}</label>
-        </div>
-    ) 
+        <div className='container'>
+            <img className='playlist-image' src={image} alt={title}/>
+                <div className='playlist-content'>
+                    <h1 className='playlist-artist'>{artists}</h1>
+                    <h4 className='playlist-album'>{albums}</h4>
+                    <h3 className='playlist-title'>{title}</h3>
+                    <button type="button" className="playlist-action"> Select </button>
+                </div>
+                    
+          </div>
+      );
 }
-
 
 export default Dataku;
